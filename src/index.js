1917c9bb207b4b863edf03a4abcea5f921c19059
H-Ramalan <talktoramalan@gmail.com>
2023-06-30 09:56:22 -0700

    post and get data from api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-import './index.css';
+import "./index.css";
+import { displayScore, addScore } from "./modules/leaderboardApi.js";
+
+const scores = document.getElementById("scores");
+const refresh = document.getElementById("refresh");
+// const submitForm = document.getElementById("score-form");
+
+refresh.addEventListener("click", () => {
+  scores.innerHTML = "";
+  displayScore();
+});
+
+addScore();
+displayScore();
---
+++ src/modules/leaderboardApi.js
@@ -0,0 +1,48 @@
+const url =
+  "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/hamza3Zl4d7IVkemOTTVg2fUdz/scores/";
+
+const scores = document.getElementById("scores");
+const addScore = () => {
+  const submitForm = document.querySelector(".score-form");
+  submitForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const username = document.getElementById("user").value;
+    const userscore = document.getElementById("score").value;
+    try {
+      await fetch(url, {
+        method: "POST",
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          user: username,
+          score: userscore,
+        }),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+    document.getElementById("user").value = "";
+    document.getElementById("score").value = "";
+  });
+};
+
+const listScore = (user, score) =>
+  `<div class="scores-list">${user}:${score}</div>`;
+const displayScore = async () => {
+  try {
+    await fetch(url)
+      .then((result) => result.json())
+      .then((datas) => {
+        datas.result.forEach((player) => {
+          const insertHTML = listScore(player.user, player.score);
+          scores.insertAdjacentHTML("afterbegin", insertHTML);
+        });
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { displayScore, addScore };
